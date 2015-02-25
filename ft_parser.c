05f6c24562cc8cf1b4fe94b06fdfbc4671390ec3
bsautron <sautron.brunojj@gmail.com>
2015-02-25 12:34:25 +0100

    pirufhserifhu

--- ft_parser.c
+++ ft_parser.c
@@ -6,37 +6,43 @@
 /*   By: bsautron <bsautron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/25 11:06:49 by bsautron          #+#    #+#             */
-/*   Updated: 2015/02/25 11:26:49 by bsautron         ###   ########.fr       */
+/*   Updated: 2015/02/25 12:33:39 by bsautron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minishell.h"
 
+static char	*get_name_var(char *str)
+{
+	size_t	i;
+
+	i = 1;
+	while (str[i] && ft_isalnum(str[i]))
+		i++;
+	return (ft_strsub(str, 0, i));
+}
+
 char	*ft_parser(char *cmd, t_lstl *env)
 {
 	char	*var;
+	char	*var_eq;
 	size_t	i;
-	size_t	j;
 	char	*var_env;
 
 	i = 0;
 	while (cmd[i])
 	{
-		j = 0;
-		if (cmd[i] == '\\')
-			i++;
-		else if (cmd[i] == '$')
+		if (cmd[i] == '$')
 		{
-			while (cmd[i + j] && cmd[i + j] != ' ')
-				j++;
-			var = ft_strsub(cmd, i, j);
-			if ((var_env = ft_get_env(ft_strjoin(var + 1, "="), env)) != NULL)
-				cmd = ft_replace(cmd, var, var_env, i);
-			else
-				cmd = ft_replace(cmd, var, "", i);
-
+			if (i == 0 || (i != 0 && cmd[i - 1] != '\\'))
+			{
+				var = get_name_var(&cmd[i]);
+				if ((var_env = ft_get_env(ft_strjoin(var + 1, "="), env)) == NULL)
+					var_env = "";
+				cmd = ft_replace(cmd, var, var_env, (i == 0) ? 0 : i - 1);
+			}
 		}
-		i += 1 + j;
+		i += 1;
 	}
 	return (cmd);
 }
