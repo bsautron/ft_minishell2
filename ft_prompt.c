4cf17ffec04ebe26a54a5d4dee4b18a91141bb07
bsautron <sautron.brunojj@gmail.com>
2015-02-26 06:18:02 +0100

    paiwuehfpwiuefh

--- ft_prompt.c
+++ ft_prompt.c
@@ -6,7 +6,7 @@
 /*   By: bsautron <bsautron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/24 17:36:20 by bsautron          #+#    #+#             */
-/*   Updated: 2015/02/26 05:25:30 by bsautron         ###   ########.fr       */
+/*   Updated: 2015/02/26 06:17:01 by bsautron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,13 +40,7 @@ static t_lstld	*ft_get_link_by_id(t_lstld *list, size_t id)
 	return (list);
 }
 
-static void		ft_home(t_env *env, size_t *pos)
-{
-	while ((*pos)++ < ft_lstl_len(env->cmd))
-		ft_make_instruction("le", NULL);
-	*pos = ft_lstl_len(env->cmd);
-	ft_make_instruction("im", NULL);
-}
+
 
 static void		ft_print_list_char(t_lstl *cmd)
 {
@@ -63,6 +57,131 @@ static void		ft_print_list_char(t_lstl *cmd)
 	}
 	tmp[i] = '\0';
 	ft_putstr(ft_reverse(tmp));
+}
+
+static void		ft_ctrl_d(t_env *env, size_t *pos)
+{
+	if (ft_lstl_len(env->cmd) == 0)
+	{
+		ft_reset_term();
+		exit(0);
+	}
+	else
+	{
+		ft_make_instruction("dc", NULL);
+		if (*pos)
+		{
+			(*pos)--;
+			ft_lstl_delone_by_id(&env->cmd, *pos);
+		}
+	}
+}
+
+static void		ft_key_home(t_env *env, size_t *pos)
+{
+	while ((*pos)++ < ft_lstl_len(env->cmd))
+		ft_make_instruction("le", NULL);
+	*pos = ft_lstl_len(env->cmd);
+	ft_make_instruction("im", NULL);
+}
+
+static void		ft_key_up(t_env *env, size_t *pos)
+{
+	if (env->h_pos == 0)
+		env->cmd_saved = env->cmd;
+	if (env->h_pos < ft_lstld_len(env->history))
+	{
+		ft_key_home(env, pos);
+		ft_make_instruction("ei", NULL);
+		ft_make_instruction("cd", NULL);
+		ft_putstr(ft_get_link_by_id(env->history, env->h_pos)->str);
+		env->cmd = ft_str_to_lstl(ft_get_link_by_id(env->history, env->h_pos)->str);
+		*pos = 0;
+		env->h_pos++;
+	}
+}
+
+static void		ft_key_down(t_env *env, size_t *pos)
+{
+	if (env->h_pos > 1)
+	{
+		env->h_pos--;
+		ft_key_home(env, pos);
+		ft_make_instruction("ei", NULL);
+		ft_make_instruction("cd", NULL);
+		ft_putstr(ft_get_link_by_id(env->history, env->h_pos - 1)->str);
+		env->cmd = ft_str_to_lstl(ft_get_link_by_id(env->history, env->h_pos - 1)->str);
+		*pos = 0;
+	}
+	else
+	{
+		env->h_pos = 0;
+		ft_key_home(env, pos);
+		env->cmd = env->cmd_saved;
+		ft_make_instruction("ei", NULL);
+		ft_make_instruction("cd", NULL);
+		ft_print_list_char(env->cmd);
+		*pos = 0;
+	}
+}
+
+static void		ft_key_left(t_env *env, size_t *pos)
+{
+	if (*pos < ft_lstl_len(env->cmd))
+	{
+		(*pos)++;
+		ft_make_instruction("le", NULL);
+		ft_make_instruction("im", NULL);
+	}
+}
+
+static void		ft_key_right(t_env *env, size_t *pos)
+{
+	env = env;
+	if (*pos != 0)
+	{
+		(*pos)--;
+		ft_make_instruction("nd", NULL);
+	}
+	if (*pos == 0)
+		ft_make_instruction("ei", NULL);
+}
+
+static void		ft_key_back_space(t_env *env, size_t *pos)
+{
+	if (*pos < ft_lstl_len(env->cmd))
+	{
+		ft_make_instruction("le", NULL);
+		ft_make_instruction("dc", NULL);
+		ft_lstl_delone_by_id(&env->cmd, *pos);
+	}
+}
+
+static void		ft_key_delete(t_env *env, size_t *pos)
+{
+	ft_make_instruction("dc", NULL);
+	if (*pos)
+	{
+		(*pos)--;
+		ft_lstl_delone_by_id(&env->cmd, *pos);
+	}
+}
+
+static void		ft_key_end(t_env *env, size_t *pos)
+{
+	env = env;
+	while ((*pos)--)
+		ft_make_instruction("nd", NULL);
+	*pos = 0;
+}
+
+
+
+static void		ft_key_printable(t_env *env, char *buf, size_t *pos)
+{
+	ft_putchar(buf[0]);
+	env->h_pos = 0;
+	ft_lstl_insert(&env->cmd, buf, *pos);
 }
 
 char			*ft_prompt(t_env *env)
@@ -88,113 +207,25 @@ char			*ft_prompt(t_env *env)
 			break ;
 		}
 		if (buf[0] == 4 && buf[1] == 0) // delete
-		{
-			if (ft_lstl_len(env->cmd) == 0)
-			{
-				ft_reset_term();
-				exit(0);
-			}
-			else
-			{
-				ft_make_instruction("dc", NULL);
-				if (pos)
-				{
-					pos--;
-					ft_lstl_delone_by_id(&env->cmd, pos);
-				}
-			}
-		}
+			ft_ctrl_d(env, &pos);
 		else if (buf[0] == '\033' && buf[2] == 'A') //haut
-		{
-			if (env->h_pos == 0)
-			{
-				env->cmd_saved = env->cmd;
-			}
-			if (env->h_pos < ft_lstld_len(env->history))
-			{
-				ft_home(env, &pos);
-				ft_make_instruction("ei", NULL);
-				ft_make_instruction("cd", NULL);
-				ft_putstr(ft_get_link_by_id(env->history, env->h_pos)->str);
-				env->cmd = ft_str_to_lstl(ft_get_link_by_id(env->history, env->h_pos)->str);
-				pos = 0;
-				env->h_pos++;
-			}
-		}
+			ft_key_up(env, &pos);
 		else if (buf[0] == '\033' && buf[2] == 'B') // bas
-		{
-			if (env->h_pos > 1)
-			{
-				env->h_pos--;
-				ft_home(env, &pos);
-				ft_make_instruction("ei", NULL);
-				ft_make_instruction("cd", NULL);
-				ft_putstr(ft_get_link_by_id(env->history, env->h_pos - 1)->str);
-				env->cmd = ft_str_to_lstl(ft_get_link_by_id(env->history, env->h_pos - 1)->str);
-				pos = 0;
-			}
-			else
-			{
-				env->h_pos = 0;
-				ft_home(env, &pos);
-				env->cmd = env->cmd_saved;
-				ft_make_instruction("ei", NULL);
-				ft_make_instruction("cd", NULL);
-				ft_print_list_char(env->cmd);
-				pos = 0;
-			}
-		}
+			ft_key_down(env, &pos);
 		else if (buf[0] == '\033' && buf[2] == 'D') //gauche
-		{
-			if (pos < ft_lstl_len(env->cmd))
-			{
-				pos++;
-				ft_make_instruction("le", NULL);
-				ft_make_instruction("im", NULL);
-			}
-		}
+			ft_key_left(env, &pos);
 		else if (buf[0] == '\033' && buf[2] == 'C') //droite
-		{
-			if (pos != 0)
-			{
-				pos--;
-				ft_make_instruction("nd", NULL);
-			}
-			if (pos == 0)
-				ft_make_instruction("ei", NULL);
-		}
+			ft_key_right(env, &pos);
 		else if (buf[0] == 8 || buf[0] == 127) //touche effacer
-		{
-			if (pos < ft_lstl_len(env->cmd))
-			{
-				ft_make_instruction("le", NULL);
-				ft_make_instruction("dc", NULL);
-				ft_lstl_delone_by_id(&env->cmd, pos);
-			}
-		}
+			ft_key_back_space(env, &pos);
 		else if (buf[0] == '\033' && buf[2] == '3' && buf[3] == '~') // touche delete
-		{
-			ft_make_instruction("dc", NULL);
-			if (pos)
-			{
-				pos--;
-				ft_lstl_delone_by_id(&env->cmd, pos);
-			}
-		}
+			ft_key_delete(env, &pos);
 		else if (buf[0] == '\033' && buf[1] == 91 && buf[2] == 70) // touche end
-		{
-			while (pos--)
-				ft_make_instruction("nd", NULL);
-			pos = 0;
-		}
+			ft_key_end(env, &pos);
 		else if (buf[0] == '\033' && buf[1] == 91 && buf[2] == 72) // touche Home
-			ft_home(env, &pos);
+			ft_key_home(env, &pos);
 		else if (ft_isprint(buf[0]) && buf[1] == 0 && buf[2] == 0 && buf[4] == 0) // une lettre printable
-		{
-			ft_putchar(buf[0]);
-			env->h_pos = 0;
-			ft_lstl_insert(&env->cmd, buf, pos);
-		}
+			ft_key_printable(env, buf, &pos);
 	}
 	the_cmd = (char *)malloc(sizeof(char) * (ft_lstl_len(env->cmd) + 1));
 	i = 0;
