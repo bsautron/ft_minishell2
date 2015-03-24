247075d8475b9b581924aa8f243ae76420e9397a
bsautron <sautron.brunojj@gmail.com>
2015-03-24 07:39:21 +0100

    io;erjgeirg

--- ft_key_right.c
+++ ft_key_right.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_key_right.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsautron <bsautron@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/03/24 07:37:44 by bsautron          #+#    #+#             */
+/*   Updated: 2015/03/24 07:38:21 by bsautron         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_minishell.h"
 
 void		ft_key_right(int *pos)
@@ -9,4 +21,8 @@ void		ft_key_right(int *pos)
 	}
 	if (*pos == 0)
 		ft_make_instruction("ei", NULL);
+	if ((ft_lstl_len(g_env.cmd) - *pos + ft_strlen("DatPrompt> ")) % (g_env.win_col - 1) == 0)
+	{
+		ft_make_instruction("do", NULL);
+	}
 }
