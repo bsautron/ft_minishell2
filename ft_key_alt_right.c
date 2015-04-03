041c681e73980f75d0fcdb0b605ad1f2b212b06a
Bruno SAUTRON <bsautron@e3r8p10.42.fr>
2015-04-03 02:37:40 +0200

    eriugh

--- ft_key_alt_right.c
+++ ft_key_alt_right.c
@@ -6,7 +6,7 @@
 /*   By: bsautron <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/04/01 01:57:51 by bsautron          #+#    #+#             */
-/*   Updated: 2015/04/01 02:35:45 by bsautron         ###   ########.fr       */
+/*   Updated: 2015/04/03 02:36:33 by bsautron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,26 @@
 
 void	ft_key_alt_right(int *pos)
 {
-	while (*pos && ft_lstl_get_link_by_id(g_env.cmd, *pos - 1)->str[0] == ' ')
+	if (*pos)
 	{
-		if ((ft_lstl_len(g_env.cmd) - *pos + ft_strlen("DatPrompt> ")) % (g_env.win_col - 1) == 0)
-			ft_make_instruction("do", NULL);
-		ft_make_instruction("nd", NULL);
-		(*pos)--;
+		while (*pos - 1 && ft_lstl_get_link_by_id(g_env.cmd, *pos - 1)->str[0] == ' ')
+		{
+			(*pos)--;
+			ft_make_instruction("nd", NULL);
+			if ((ft_lstl_len(g_env.cmd) - *pos + ft_strlen("DatPrompt> ")) % (g_env.win_col - 1) == 0)
+				ft_make_instruction("do", NULL);
+		}
+		while (*pos - 1 && ft_lstl_get_link_by_id(g_env.cmd, *pos - 1)->str[0] != ' ')
+		{
+			(*pos)--;
+			ft_make_instruction("nd", NULL);
+			if ((ft_lstl_len(g_env.cmd) - *pos + ft_strlen("DatPrompt> ")) % (g_env.win_col - 1) == 0)
+				ft_make_instruction("do", NULL);
+		}
 	}
-
-	while (*pos && ft_lstl_get_link_by_id(g_env.cmd, *pos - 1)->str[0] != ' ')
+	if (*pos == 1)
 	{
-		if ((ft_lstl_len(g_env.cmd) - *pos + ft_strlen("DatPrompt> ")) % (g_env.win_col - 1) == 0)
-			ft_make_instruction("do", NULL);
-		ft_make_instruction("nd", NULL);
 		(*pos)--;
+		ft_make_instruction("nd", NULL);
 	}
 }
