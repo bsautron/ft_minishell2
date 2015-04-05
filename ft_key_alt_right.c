385aa56e84b2705746eb6162f556c3a3a5b43911
Bruno SAUTRON <bsautron@e3r9p16.42.fr>
2015-04-05 06:37:01 +0200

    iapurhserg

--- ft_key_alt_right.c
+++ ft_key_alt_right.c
@@ -6,11 +6,17 @@
 /*   By: bsautron <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/04/01 01:57:51 by bsautron          #+#    #+#             */
-/*   Updated: 2015/04/05 06:27:36 by bsautron         ###   ########.fr       */
+/*   Updated: 2015/04/05 06:36:56 by bsautron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_minishell.h>
+
+static void	ft_doo(int *pos)
+{
+	if ((ft_lstl_len(g_env.cmd) - *pos + ft_strlen(g_env.prompt)) % (g_env.win_col - 1) == 0)
+		ft_make_instruction("do", NULL);
+}
 
 void	ft_key_alt_right(int *pos)
 {
@@ -20,20 +26,19 @@ void	ft_key_alt_right(int *pos)
 		{
 			(*pos)--;
 			ft_make_instruction("nd", NULL);
-			if ((ft_lstl_len(g_env.cmd) - *pos + ft_strlen(g_env.prompt)) % (g_env.win_col - 1) == 0)
-				ft_make_instruction("do", NULL);
+			ft_doo(pos);
 		}
 		while (*pos - 1 && ft_lstl_get_link_by_id(g_env.cmd, *pos - 1)->str[0] != ' ')
 		{
 			(*pos)--;
 			ft_make_instruction("nd", NULL);
-			if ((ft_lstl_len(g_env.cmd) - *pos + ft_strlen(g_env.prompt)) % (g_env.win_col - 1) == 0)
-				ft_make_instruction("do", NULL);
+			ft_doo(pos);
 		}
 	}
 	if (*pos == 1)
 	{
 		(*pos)--;
 		ft_make_instruction("nd", NULL);
+		ft_doo(pos);
 	}
 }
