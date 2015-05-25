27e36b3430841a662b814047283a59abe1d4de91
Your Name <you@example.com>
2015-05-25 19:08:13 +0200

    eroguihserg

--- scope/ft_scope_default.c
+++ scope/ft_scope_default.c
@@ -6,7 +6,7 @@
 /*   By: bsautron <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/23 19:03:31 by bsautron          #+#    #+#             */
-/*   Updated: 2015/05/25 18:54:28 by bsautron         ###   ########.fr       */
+/*   Updated: 2015/05/25 19:05:50 by bsautron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 
 void	ft_scope_default(char c)
 {
+	if (c == '\'')
+		ft_scope_push(&g_env.scope, SCOPE_QUOTE);
 	if (c == '"')
 		ft_scope_push(&g_env.scope, SCOPE_DQUOTE);
+	if (c == '`')
+		ft_scope_push(&g_env.scope, SCOPE_BQUOTE);
+	if (c == '(')
+		ft_scope_push(&g_env.scope, SCOPE_SUBSH);
+	if (c == '{')
+		ft_scope_push(&g_env.scope, SCOPE_CURSH);
+	if (c == '[')
+		ft_scope_push(&g_env.scope, SCOPE_BQUOTE);
 }
