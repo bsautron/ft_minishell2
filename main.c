a3a4dd1c530bdaca912312f91cede48d02bb60e9
Bruno <sautron.brunojj@gmail.com>
2015-05-29 22:00:01 +0200

    s;oeirgjserg

--- main.c
+++ main.c
@@ -4,17 +4,15 @@
 int		main(int ac, char **av, char **env)
 {
 	char	*cmd;
-	int		status;
 	t_btree	*tree;
 
-	while (1)
-	{
-		cmd = ft_get_cmd(env);
-		tree = lex_and_parse(cmd, &status);
-		print_tree(tree, 3);
-		dprintf(1, "%s\n", "");
-	}
+	// Rajouter une la condition que si cest la mm cmd, de ne pas ecrire dans le fichier
+	dprintf(1, "%s\n", "begin");
+	cmd = ft_get_cmd(env);
+	tree = lex_and_parse(cmd);
+	print_tree(tree, 3);
 
+	(void)env;
 	(void)av;
 	(void)ac;
 	return (0);
