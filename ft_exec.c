c0f23d9ab2abacd99eb33f8bd8406fdf22388c9f
Your Name <you@example.com>
2015-06-09 23:42:30 +0200

    egrsuerg

--- ft_exec.c
+++ ft_exec.c
@@ -6,9 +6,22 @@
 /*   By: bsautron <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/09 22:45:19 by bsautron          #+#    #+#             */
-/*   Updated: 2015/06/09 22:45:19 by bsautron         ###   ########.fr       */
+/*   Updated: 2015/06/09 23:42:15 by bsautron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <>
+#include <ft_sh.h>
 
+#define CHILD		0
+#define PARENT		!CHILD
+
+
+int		ft_exec(char *bin, char **tab, char **env)
+{
+	pid_t	child;
+
+	child = fork();
+	(void)bin;
+	(void)tab;
+	(void)env;
+}
